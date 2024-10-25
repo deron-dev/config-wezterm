26417edfa7f9059424cfbe8405a4a4b455413a21
Deron Parker <24536673+deron-dev@users.noreply.github.com>
2024-10-25 01:46:52 -0500

    init

---
+++ wezterm.lua
@@ -0,0 +1,45 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.default_prog = { 'pwsh' }
+config.color_scheme = 'Oxocarbon Dark (Gogh)'
+
+-- config.window_decorations = "NONE";
+config.hide_tab_bar_if_only_one_tab = true;
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+    {
+        key = 'b',
+        mods = 'LEADER|CTRL',
+        action = wezterm.action.SendKey { key = 'b', mods = 'CTRL' },
+    },
+    {
+        key = '"',
+        mods = 'LEADER|SHIFT',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = '%',
+        mods = 'LEADER|SHIFT',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    { key = "c", mods = "LEADER", action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+    { key = "h", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Left" } },
+    { key = "j", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Down" } },
+    { key = "k", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Up" } },
+    { key = "l", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Right" } },
+    { key = "1", mods = "ALT",    action = wezterm.action { ActivateTab = 0 } },
+    { key = "2", mods = "ALT",    action = wezterm.action { ActivateTab = 1 } },
+    { key = "3", mods = "ALT",    action = wezterm.action { ActivateTab = 2 } },
+    { key = "4", mods = "ALT",    action = wezterm.action { ActivateTab = 3 } },
+    { key = "5", mods = "ALT",    action = wezterm.action { ActivateTab = 4 } },
+    { key = "6", mods = "ALT",    action = wezterm.action { ActivateTab = 5 } },
+    { key = "7", mods = "ALT",    action = wezterm.action { ActivateTab = 6 } },
+    { key = "8", mods = "ALT",    action = wezterm.action { ActivateTab = 7 } },
+    { key = "9", mods = "ALT",    action = wezterm.action { ActivateTab = 8 } },
+};
+
+
+return config
